812ea5e0c289027f5b20743121106ad702855940
Paz Manrique <nautilus1985@gmail.com>
2022-11-14 18:24:37 +0100

    Creating a class and variables

--- src/com/openbootcamp/Main.java
+++ src/com/openbootcamp/Main.java
@@ -1,2 +1,16 @@
-package com.openbootcamp;public class Main {
+package com.openbootcamp;
+
+public class Main {
+    public static void main(String[] args) {
+        int amount = 20;
+        long id = 45;
+        double price = 30.3d;
+        String name = "Silla";
+        System.out.println("La cantidad es " + amount);
+        System.out.println("El id es " + id);
+        System.out.println("El precio es " + price);
+        System.out.println("El nombre es " + name);
+
+
+    }
 }
