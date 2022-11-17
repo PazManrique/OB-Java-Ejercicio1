d5d11f422f14a3c549f087a26ff70cf02b8702f5
Paz Manrique <nautilus1985@gmail.com>
2022-11-17 11:09:50 +0100

    Creating a bucle

---
+++ src/com/openbootcamp/Bucles.java
@@ -0,0 +1,10 @@
+package com.openbootcamp;
+
+public class Bucles {
+    public static void main(String[] args) {
+        String[] names = {"La", "navidad", "ya", "esta", "cerca"};
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
